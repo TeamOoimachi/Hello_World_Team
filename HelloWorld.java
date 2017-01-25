e5f4a2ae8e061e7d914a2074fe37d7147fb57aa9
OoimachiTestUser <n.ooimachi.test.user@gmail.com>
2017-01-25 13:02:17 +0900

    ソースコードを追加しました。

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloClass {
     public static void main(String[] args){
         System.out.println("Hello, world.");
+
+        System.out.println("‚Í‚¶‚ß‚Ä‚Ìƒuƒ‰ƒ“ƒ`ì¬");
     }
 }
